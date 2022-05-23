ade32636ee43d2ab0dee70867b02e162c6960d12
brunomaria71 <brunomaria71@yahoo.com>
2022-05-23 11:28:52 -0400

    added nav and routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "antd";
 import RestaurantList from "./components/RestaurantList";
 import Menubar from "./components/Menubar";
@@ -7,14 +8,22 @@ const { Header, Content } = Layout;
 
 function App() {
   return (
+    <BrowserRouter>
     <Layout className="layout">
       <Header>
      <Menubar />
       </Header>
       <Content>
-        <RestaurantList />
+        <Routes>
+        <Route path='/home' element={ <RestaurantList />}/>
+        <Route path='/random' element={ <RestaurantList />}/>
+        <Route path='/add' element={ <RestaurantList />}/>
+        <Route path='/login' element={ <RestaurantList />}/>
+        <Route path='/' element={ <RestaurantList />}/>
+        </Routes>
       </Content>
     </Layout>
+    </BrowserRouter>
   );
 }
 
--- src/components/Menubar.js
+++ src/components/Menubar.js
@@ -1,25 +1,38 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { Menu } from "antd";
 import {
   PlusCircleOutlined,
   QuestionCircleOutlined,
   UserOutlined,
+  HomeOutlined,
 } from "@ant-design/icons";
 
 
 export default function Menubar(){
+    let navigate = useNavigate()
+
     return (
         <Menu theme="dark" mode="horizontal">
+          <Menu.Item
+          key="home"
+          onClick={() => navigate('/')}
+          icon={<HomeOutlined style={{ fontSize: "1.8em" }} />}
+        />
         <Menu.Item
           key="add"
+          onClick={() => navigate('/add')}
+
           icon={<PlusCircleOutlined style={{ fontSize: "1.8em" }} />}
         />
         <Menu.Item
-          key="add"
+          key="random"
           icon={<QuestionCircleOutlined style={{ fontSize: "1.8em" }} />}
         />
         <Menu.Item
-          key="add"
+          key="user"
+          onClick={() => navigate('/login')}
+
           icon={<UserOutlined style={{ fontSize: "1.8em" }} />}
         />
       </Menu>
